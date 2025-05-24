a6ee58e31d3be792a2651f77b28acbcc2c08d16a
ZEROAUL_Mohammed <zeroual.md1@gmail.com>
2025-05-25 00:02:51 +0200

    architecture front de la page inscription

--- inscription.php
+++ inscription.php
@@ -32,26 +32,26 @@
 
 
             <label for="nomComplet">Nom complet</label>
-            <input type="text" id="nomComplet" name="nomComplet" required />
+            <input type="text" id="nomComplet" name="nomComplet" placeholder="Mohammed ZEROUAL" required />
 
             <label for="pseudo">Pseudo</label>
-            <input type="text" id="pseudo" name="pseudo" required />
+            <input type="text" id="pseudo" name="pseudo" placeholder="Midou92" required />
 
             <label for="telephone">Téléphone</label>
-            <input type="tel" id="telephone" name="telephone" required pattern="[0-9]{10}" placeholder="Ex: 0612345678" />
+            <input type="tel" id="telephone" name="telephone" required pattern="[0-9]{10}" placeholder="0612345678" />
 
-            <label for="email">Email</label>
-            <input type="email" id="email" name="email" required />
+            <label for="email" >Email</label>
+            <input type="email" id="email" name="email" placeholder="midou@gmail.com" required />
 
-            <label for="ville">Ville</label>
-            <input type="text" id="ville" name="ville" required />
+            <label for="ville" >Ville</label>
+            <input type="text" id="ville" name="ville" placeholder="Paris" required />
 
-            <label for="annee">Année de naissance :</label>
-            <input type="number" name="annee" id="annee" min="1900" max="<?php echo date('Y')-18; ?>" step="1">
+            <label for="annee" >Année de naissance :</label>
+            <input type="number" name="annee" id="annee" min="1900" max="<?php echo date('Y')-18; ?>" step="1" placeholder="2004">
 
-            <label for="motdepasse">Mot de passe</label>
+            <label for="motdepasse" >Mot de passe</label>
                 <div class="password-wrapper">
-                    <input type="password" id="motdepasse" name="motdepasse" required minlength="6" />
+                    <input type="password" id="motdepasse" name="motdepasse" required minlength="6"  placeholder="Entrer votre mot de passe..." />
                 </div>
                 <div>   
                      <button type="button" id="togglePassword" aria-label="Afficher le mot de passe" title="Afficher / Cacher" class="toggle-btn">Afficher mon mot de passe</button>
